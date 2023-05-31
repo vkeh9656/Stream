25763054e7e44bac2fccae1f109e9fbedb00ccff
vkeh9656 <37315637+vkeh9656@users.noreply.github.com>
2023-05-31 21:56:33 +0900

    UI 세팅 및 이벤트 구현

--- Stream/Form1.Designer.cs
+++ Stream/Form1.Designer.cs
@@ -28,13 +28,180 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.tboxConfigData = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numData = new System.Windows.Forms.NumericUpDown();
+            this.cboxData = new System.Windows.Forms.CheckBox();
+            this.tboxData = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConfigSet = new System.Windows.Forms.Button();
+            this.btnConfigRead = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(12, 12);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(131, 23);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Text 읽어오기";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(149, 12);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(131, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Text 저장하기";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // tboxConfigData
+            // 
+            this.tboxConfigData.Enabled = false;
+            this.tboxConfigData.Location = new System.Drawing.Point(12, 41);
+            this.tboxConfigData.Multiline = true;
+            this.tboxConfigData.Name = "tboxConfigData";
+            this.tboxConfigData.Size = new System.Drawing.Size(268, 119);
+            this.tboxConfigData.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numData);
+            this.groupBox1.Controls.Add(this.cboxData);
+            this.groupBox1.Controls.Add(this.tboxData);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 176);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(268, 118);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Config Setting";
+            // 
+            // numData
+            // 
+            this.numData.Location = new System.Drawing.Point(95, 83);
+            this.numData.Name = "numData";
+            this.numData.Size = new System.Drawing.Size(80, 21);
+            this.numData.TabIndex = 5;
+            // 
+            // cboxData
+            // 
+            this.cboxData.AutoSize = true;
+            this.cboxData.Location = new System.Drawing.Point(112, 56);
+            this.cboxData.Name = "cboxData";
+            this.cboxData.Size = new System.Drawing.Size(47, 16);
+            this.cboxData.TabIndex = 4;
+            this.cboxData.Text = "CB1";
+            this.cboxData.UseVisualStyleBackColor = true;
+            // 
+            // tboxData
+            // 
+            this.tboxData.Location = new System.Drawing.Point(75, 25);
+            this.tboxData.Name = "tboxData";
+            this.tboxData.Size = new System.Drawing.Size(100, 21);
+            this.tboxData.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "3. Number :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2. Check Box :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1. Text :";
+            // 
+            // btnConfigSet
+            // 
+            this.btnConfigSet.Location = new System.Drawing.Point(149, 300);
+            this.btnConfigSet.Name = "btnConfigSet";
+            this.btnConfigSet.Size = new System.Drawing.Size(131, 23);
+            this.btnConfigSet.TabIndex = 5;
+            this.btnConfigSet.Text = "Config 설정하기";
+            this.btnConfigSet.UseVisualStyleBackColor = true;
+            this.btnConfigSet.Click += new System.EventHandler(this.btnConfigSet_Click);
+            // 
+            // btnConfigRead
+            // 
+            this.btnConfigRead.Location = new System.Drawing.Point(12, 300);
+            this.btnConfigRead.Name = "btnConfigRead";
+            this.btnConfigRead.Size = new System.Drawing.Size(131, 23);
+            this.btnConfigRead.TabIndex = 4;
+            this.btnConfigRead.Text = "Config 가져오기";
+            this.btnConfigRead.UseVisualStyleBackColor = true;
+            this.btnConfigRead.Click += new System.EventHandler(this.btnConfigRead_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(295, 332);
+            this.Controls.Add(this.btnConfigSet);
+            this.Controls.Add(this.btnConfigRead);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tboxConfigData);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox tboxConfigData;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numData;
+        private System.Windows.Forms.CheckBox cboxData;
+        private System.Windows.Forms.TextBox tboxData;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConfigSet;
+        private System.Windows.Forms.Button btnConfigRead;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
     }
 }
 
--- Stream/Form1.cs
+++ Stream/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,80 @@ namespace Stream
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnConfigSet_Click(object sender, EventArgs e)
+        {
+            string strEnter = "\r\n";
+
+            string strText = tboxData.Text;
+            bool bChecked = cboxData.Checked;
+            int iNumber = (int)numData.Value;
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(strText + strEnter);
+            sb.Append(bChecked.ToString() + strEnter);
+            sb.Append(iNumber.ToString() + strEnter);
+
+            tboxConfigData.Text = sb.ToString();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string strFilePath = string.Empty;
+
+            saveFileDialog.InitialDirectory = Application.StartupPath; // 프로그램 실행 파일 위치
+            saveFileDialog.FileName = "*.txt";
+            saveFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if(saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                strFilePath = saveFileDialog.FileName;
+                
+                //StreamWriter swSaveFileDialog = new StreamWriter(strFilePath);
+
+                //swSaveFileDialog.WriteLine(tboxConfigData.Text);
+                //swSaveFileDialog.Close();
+
+                File.WriteAllText(strFilePath, tboxData.Text); // 위 3줄 주석과 동일한 기능임.
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            string strFilePath = string.Empty;
+
+            openFileDialog.InitialDirectory = Application.StartupPath;
+            openFileDialog.FileName = "*.txt";
+            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            StringBuilder sb = new StringBuilder();
+
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                strFilePath = openFileDialog.FileName;
+
+                //StreamReader srOpenFileDialog = new StreamReader(strFilePath, Encoding.UTF8, true);
+
+                //while(srOpenFileDialog.EndOfStream == false)
+                //{
+                //    sb.Append(srOpenFileDialog.ReadLine());
+                //    sb.Append("\r\n");
+                //}
+
+                sb.Append(File.ReadAllText(strFilePath)); // 위 5줄과 동일한 기능
+
+                tboxConfigData.Text = sb.ToString();
+            }
+        }
+
+        private void btnConfigRead_Click(object sender, EventArgs e)
+        {
+            string[] strConfig = tboxConfigData.Text.Split(new char[] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
+
+            tboxData.Text = strConfig[0];
+            cboxData.Checked = bool.Parse(strConfig[1]);
+            numData.Value = int.Parse(strConfig[2]);
         }
     }
 }
